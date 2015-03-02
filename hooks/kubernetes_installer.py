aa389a306e96038002ea8a5fd1ca22676bc16beb
Matt Bruzek <mbruzek@gmail.com>
2015-03-02 17:35:26 -0600

    Changing the installer code to download the binaries from the kubernetes master.

--- hooks/kubernetes_installer.py
+++ hooks/kubernetes_installer.py
@@ -9,59 +9,41 @@ class KubernetesInstaller():
     from a tar file or by using gsutil.
     """
 
-    def __init__(self, arch, version, kubernetes_file):
+    def __init__(self, arch, version, master):
         """ Gather the required variables for the install. """
         # The kubernetes charm needs certain commands to be aliased.
         self.aliases = {'kube-proxy': '/usr/local/bin/proxy',
                         'kubelet': '/usr/local/bin/kubelet'}
         self.arch = arch
         self.version = version
-        self.kubernetes_file = path(kubernetes_file)
+        self.master = master
 
-    def install(self, output_dir=path('/opt/kubernetes/bin')):
-        """ Install kubernetes binary files from the tar file or gsutil. """
-        if output_dir.isdir():
-            # Remote old content to remain idempotent.
-            output_dir.rmtree()
+    def download(self, download_dir=path('/opt/kubernetes/bin')):
+        """ Download the kuberentes binaries from the kubernetes master. """
+        url = 'http://{0}:8080/kubernetes/{1}/bin/linux/{2}/'.format(
+            self.master, self.version, self.arch)
+        if not download_dir.exists():
+            download_dir.makedirs_p()
+        for key in self.aliases:
+            uri = '{0}/{1}'.format(url, key)
+            wget = 'wget -nv {0} -O {1}'.format(uri, self.download_dir)
+        version = download_dir / '.version'
+        version.write_text(self.version)
 
-        # Create the output directory.
-        output_dir.makedirs()
+    def install(self, install_dir=path('/usr/local/bin')):
+        """ Copy the binary files to the install directory. """
 
-        # Write the version and arch to a dot file.
-        (output_dir / '.kubernetes').write_text('{0} {1}'.format(self.version,
-                                                                 self.arch))
-
-        if self.kubernetes_file.exists():
-            # Untar the file to the output directory.
-            command = 'tar -xvzf {0} -C {1}'.format(self.kubernetes_file,
-                                                    output_dir)
-            print(command)
-            output = subprocess.check_output(command, shell=True)
-            print(output)
-        else:
-            # Get the binaries from the gsutil command.
-            self.get_kubernetes_gsutil(output_dir)
+        if not install_dir.exists()
+            install_dir.makedirs_p()
 
         # Create the symbolic links to the real kubernetes binaries.
-        usr_local_bin = path('/usr/local/bin')
         for key, value in self.aliases.iteritems():
-            target = output_dir / key
-            link = usr_local_bin / value
-            if link.exists():
-                link.remove()
-            target.symlink(link)
-
-    def get_kubernetes_gsutil(self, directory):
-        """ Download the kubernetes binary objects from gsutil. """
-        uri = 'gs://kubernetes-release/release/{0}/bin/linux/{1}/'.format(
-              self.version, self.arch)
-        gs_command = 'gsutil cp {0} {1}'
-        # Download all the keys in the aliases dictionary to this machine.
-        for key in self.aliases:
-            # Create the remote target by appending the key to the uri string.
-            remote = uri + key
-            # Create the local path and file name string.
-            local = os.path.join(directory, key)
-            command = gs_command.format(remote, local)
-            print(command)
-            subprocess.check_call(command.split())
+            target = self.output_dir / key
+            if target.exists():
+                link = install_dir / value
+                if link.exists():
+                    link.remove()
+                target.symlink(link)
+            else:
+                print('Error target file {0} does not exist.'.format(target))
+                exit(1)
